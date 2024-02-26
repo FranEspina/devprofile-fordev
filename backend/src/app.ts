83a7f6235bbb96d3cde712434df998a3e754f7b8
Francisco José Espina Aragón <francisco.espina.aragon@gmail.com>
2024-02-26 16:40:47 +0100

    cors development

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,7 @@ import authRoute from './routes/authRoute'
 const app = express()
 
 const corsOptions = {
-  origin: 'http://localhost:8081'
+  origin: 'http://localhost:4321'
 }
 
 app.use(cors(corsOptions))

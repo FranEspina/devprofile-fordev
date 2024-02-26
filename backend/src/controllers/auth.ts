70fc864e4d9e70b87ef5ea07872e5e4370592aa9
Francisco José Espina Aragón <francisco.espina.aragon@gmail.com>
2024-02-26 13:46:46 +0100

    encriptacion

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -11,6 +11,7 @@ export const register: RequestHandler = async (req, res) => {
   if (isEmailAllReadyExist) {
     res.status(400).json({
       status: 400,
+      success: false,
       code: 'EMAIL_EXIST',
       message: 'El e-mail ya está en uso',
     })
@@ -25,5 +26,23 @@ export const register: RequestHandler = async (req, res) => {
     message: 'Usuario registrado correctamente',
     data: newUser,
   })
+}
+
+export const login: RequestHandler = async (req, res) => {
+
+  const user = req.body;
+  const { email, password } = user;
+
+  const isUserExist = await getUserByEmail(email)
+  if (!isUserExist) {
+    res.status(400).json({
+      success: false,
+      status: 400,
+      code: 'USER_NOT_FOUND',
+      message: 'Usuario no encontrado',
+    })
+    return
+  }
+
 }
 
---
+++ backend/src/services/cryptService.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcryptjs'
+
+export async function hashAsync(value: string): Promise<string> {
+  return await bcrypt.hash(value, 10)
+}
+
+export async function compareHashAsync(value: string, valueHashed: string): Promise<boolean> {
+  console.log(valueHashed)
+  return await bcrypt.compare(value, valueHashed)
+}
+
+export async function generateRandomHashAsync() {
+  const randomKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const hashedKey = await bcrypt.hash(randomKey, 10)
+  return hashedKey
+}

5084c34f62bcb0bf936f1dec8996a894207b4d18
Francisco José Espina Aragón <francisco.espina.aragon@gmail.com>
2024-03-02 13:32:49 +0100

    modificado esquema enum

--- backend/src/schemas/devResourceSchema.ts
+++ backend/src/schemas/devResourceSchema.ts
@@ -1,11 +1,31 @@
 import { z } from 'zod'
 
+export enum ResourceType {
+  Markdown = 'markdown',
+  Imagen = 'imagen',
+  Web = 'web',
+  Archivo = 'archivo'
+}
+
+const resourceTypeValues = Object.values(ResourceType).join(', ')
+
 export const DevResourceSchema = z.object({
   id: z.number({ required_error: 'Identificador del recurso obligatorio' }),
   userId: z.number({ required_error: 'Identificador del usuario obligatorio' }),
   title: z.string({ required_error: 'Título del recurso obligatorio' }).min(1),
   description: z.string({ required_error: 'Descripción del recurso obligatorio' }).min(1),
-  type: z.enum(['markdown', 'imagen', 'web', 'archivo']),
+  type: z.nativeEnum(ResourceType, {
+    errorMap: (issue) => {
+      switch (issue.code) {
+        case 'invalid_type':
+          return { message: `El tipo de contenido debe ser: ${resourceTypeValues}.` }
+        case 'invalid_enum_value':
+          return { message: `El valor proporcionado no es válido. Debe ser uno de los siguientes: ${resourceTypeValues}.` }
+        default:
+          return { message: 'Error desconocido al validar el tipo de contenido.' }
+      }
+    }
+  }),
   url: z.string().url({ message: 'url inválida' }).optional(),
   keywords: z.string().optional()
 })

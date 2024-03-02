a8dd635958146263da292111ddd70e6eada3ba07
Francisco José Espina Aragón <francisco.espina.aragon@gmail.com>
2024-03-02 13:37:17 +0100

    validaciones cliente

--- web/src/Schemas/resourceSchema.ts
+++ web/src/Schemas/resourceSchema.ts
@@ -1,11 +1,31 @@
 import { z } from "astro/zod";
 
+export enum ResourceType {
+  Markdown = 'markdown',
+  Imagen = 'imagen',
+  Web = 'web',
+  Archivo = 'archivo'
+}
+
+const resourceTypeValues = Object.values(ResourceType).join(', ')
+
 export const ResourceSchema = z.object({
   id: z.number(),
   userId: z.number(),
   title: z.string().min(1, 'Campo obligatorio'),
   description: z.string().min(1, 'Campo obligatorio'),
-  type: z.enum(['markdown', 'imagen', 'web', 'archivo']),
+  type: z.nativeEnum(ResourceType, {
+    errorMap: (issue) => {
+      switch (issue.code) {
+        case 'invalid_type':
+          return { message: `El tipo de contenido debe ser: ${resourceTypeValues}.` }
+        case 'invalid_enum_value':
+          return { message: `El valor proporcionado no es válido. Debe ser uno de los siguientes: ${resourceTypeValues}.` }
+        default:
+          return { message: 'Error desconocido al validar el tipo de contenido.' }
+      }
+    }
+  }),
   url: z.string().optional(),
   keywords: z.string().optional()
 })

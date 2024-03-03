8f30fd44cc2a605d4e335f28294a06d6c324fad3
Francisco José Espina Aragón <francisco.espina.aragon@gmail.com>
2024-03-03 17:57:48 +0100

    eliminado classnames

--- web/src/components/Modal.jsx
+++ web/src/components/Modal.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import CloseButton from './CloseButton'
-import classNames from 'classnames';
+import { cn } from '@/lib/utils'
 
 // Modal Component
 export const Modal = ({ title, show, handleConfirm, textConfirm, handleCancel, className = '', loading, children }) => {
   return (show &&
     <div className="fixed z-10 left-0 top-0 w-full h-full overflow-auto bg-black/90">
-      <section className={classNames('bg-[#101015] my-[5%] w-[80%] mx-auto  shadow-md shadow-blue-500 flex flex-col border border-blue-500', className)}>
+      <section className={cn('bg-[#101015] my-[5%] w-[80%] mx-auto  shadow-md shadow-blue-500 flex flex-col border border-blue-500', className)}>
         <header className="relative bg-blue-800 p-2">
           <h1 className='text-gray-50 text-base md:text-lg' >{title}</h1>
           <CloseButton size='mobile' onClick={handleCancel} />
@@ -16,7 +16,7 @@ export const Modal = ({ title, show, handleConfirm, textConfirm, handleCancel, c
         </main>
         <footer className="flex flex-row justify-center gap-2 mb-4 md:mb-7 text-[10px]" >
           <button
-            className={classNames('uppercase border border-gray-50 px-2 md:px-5 py-1', loading ? '' : ' hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer hover:shadow-lg transition-colors duration-300')}
+            className={cn('uppercase border border-gray-50 px-2 md:px-5 py-1', loading ? '' : ' hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer hover:shadow-lg transition-colors duration-300')}
             onClick={handleConfirm} disabled={loading}>
             {loading &&
               <svg className="animate-spin h-5 w-5 "

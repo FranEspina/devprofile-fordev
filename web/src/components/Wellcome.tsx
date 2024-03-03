74c4b403d2d508cf838256dbee32f622dc8c4a2c
Francisco José Espina Aragón <francisco.espina.aragon@gmail.com>
2024-03-03 17:10:43 +0100

    validacion ts

--- web/src/components/Wellcome.tsx
+++ web/src/components/Wellcome.tsx
@@ -7,7 +7,7 @@ export function Wellcome() {
   return (
     <>
       <div className='m-4 text-xs md:text-base '>
-        {token && <p>{`Bienvenido ${user.firstName}`}</p>}
+        {token && user && <p>{`Bienvenido ${user.firstName}`}</p>}
         {(!token) && <p className='flex flex-row'>Para continuar ➡️<span className='mx-2'><SignInModal text='Inicia sesión' /></span></p>}
         {(!token) && <p className='flex flex-row'>También puedes ➡️<span className='mx-2'><SignUpModal text='Crear cuenta' /></span></p>}
       </div>
